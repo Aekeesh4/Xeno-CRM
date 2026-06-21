6604815f760a25415d34ff150d28329497c5983d
Aekeesh4 <aekeeshjaiswal@gmail.com>
2026-06-22 02:57:00 +0530

    Fix Railway CORS

--- src/main/java/com/xeno/xenocrm/config/CorsConfig.java
+++ src/main/java/com/xeno/xenocrm/config/CorsConfig.java
@@ -2,13 +2,12 @@ package com.xeno.xenocrm.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
@@ -20,25 +19,12 @@ public class CorsConfig {
 
         config.setAllowCredentials(true);
 
-        config.setAllowedOrigins(
-                Arrays.asList(
-                        "http://localhost:5173"
-                )
-        );
+        config.setAllowedOriginPatterns(List.of("*"));
 
-        config.setAllowedHeaders(
-                Arrays.asList("*")
-        );
+        config.setAllowedHeaders(List.of("*"));
 
         config.setAllowedMethods(
-                Arrays.asList(
-                        "GET",
-                        "POST",
-                        "PUT",
-                        "DELETE",
-                        "OPTIONS"
-                )
-        );
+                List.of("GET","POST","PUT","DELETE","OPTIONS"));
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
@@ -47,5 +33,4 @@ public class CorsConfig {
 
         return new CorsFilter(source);
     }
-
 }
--- xenocrm-frontend/src/pages/Login.jsx
+++ xenocrm-frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ function Login() {
     try {
 
       const response = await fetch(
-        "http://localhost:8081/api/auth/login",
+        "https://xeno-crm-production-1dfc.up.railway.app/api/auth/login"
         {
           method: "POST",
 
